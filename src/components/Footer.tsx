7b0460869737289f979343cfc6d966e2d081a40b
jeresc <jeremiastomassrc@gmail.com>
2024-01-28 19:14:54 -0300

    refactor: ♻ update currentYear on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
       className='flex w-full items-center justify-end gap-4 border-t-[1px] border-[#0004]
       border-t-[#e5e5e5] px-4 py-2 dark:border-[#fff4]'
     >
-      <p>© 2023 Jeremias Soruco</p>
+      <p>© {new Date().getFullYear()} Jeremias Soruco</p>
     </footer>
   )
 }
