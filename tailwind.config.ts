2c8da83f1100b6f276b63ad42d4dec974a3c5f38
jeresc <jeremiastomassrc@gmail.com>
2024-04-07 23:54:41 -0300

    fix: 💄 update style of code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,44 +1,45 @@
-import type {Config} from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme'
+import type {Config} from "tailwindcss";
+
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
-      xs: '480px',
-      '2xs': '360px',
+      xs: "480px",
+      "2xs": "360px",
       ...defaultTheme.screens,
     },
     fontFamily: {
-      montserrat: ['var(--font-montserrat)'],
-      monaSans: ['var(--font-mona-sans)'],
-      playfair: ['var(--font-playfair)'],
-      geist: ['var(--font-geist-mono)'],
+      montserrat: ["var(--font-montserrat)"],
+      monaSans: ["var(--font-mona-sans)"],
+      playfair: ["var(--font-playfair)"],
+      geist: ["var(--font-geist-mono)"],
     },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       height: {
-        '6.5': '1.625rem',
+        "6.5": "1.625rem",
       },
       colors: {
-        primary: '#fff',
-        secondary: '#121212',
-        background: 'var(--background)',
-        input: 'hsl(240,5.9%,90%)',
-        muted: 'hsl(var(--muted))',
-        border: 'var(--border)',
+        primary: "#fff",
+        secondary: "#121212",
+        background: "var(--background)",
+        input: "hsl(240,5.9%,90%)",
+        muted: "hsl(var(--muted))",
+        border: "var(--border)",
       },
     },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [],
-}
-export default config
+};
+
+export default config;
